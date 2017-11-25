aad081be8ff0f09c7b43a0609683d5e92e4bb28f
Litee <lipatkin@gmail.com>
2017-11-25 22:45:03 +0300

    Renamed tests

---
+++ Source/Moq.Analyzers.Test/CallbackSignatureAnalyzerTests.cs
@@ -0,0 +1,32 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.IO;
+using TestHelper;
+using Xunit;
+
+namespace Moq.Analyzers.Test
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class CallbackSignatureAnalyzerTests : DiagnosticVerifier
+    {
+
+        [Fact]
+        public void ShouldPassIfGoodParameters()
+        {
+            VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatures/CallbackWithGoodParameters.cs"));
+        }
+
+        [Fact]
+        public void ShouldFailIfBadParameters()
+        {
+            Approvals.Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatures/CallbackWithBadParameters.cs")));
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CallbackSignatureAnalyzer();
+        }
+    }
+}
---
+++ Source/Moq.Analyzers.Test/CallbackSignatureCodeFixTests.cs
@@ -0,0 +1,31 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.IO;
+using TestHelper;
+using Xunit;
+
+namespace Moq.Analyzers.Test
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class CallbackSignatureCodeFixTests : CodeFixVerifier
+    {
+
+        [Fact]
+        public void ShouldSuggestQuickFixIfBadParameters()
+        {
+            Approvals.Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatures/CallbackWithBadParameters.cs")));
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new CallbackSignatureCodeFix();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CallbackSignatureAnalyzer();
+        }
+    }
+}
