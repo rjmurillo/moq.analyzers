75608163d6b2c062c1b7747958ecec859c0db505
Richard Murillo <richard.murillo@microsoft.com>
2024-05-31 09:31:09 -0700

    Update DiagnosticVerifier.Helper.cs to move using inside namespace

--- Source/Moq.Analyzers.Test/Helpers/DiagnosticVerifier.Helper.cs
+++ Source/Moq.Analyzers.Test/Helpers/DiagnosticVerifier.Helper.cs
@@ -1,10 +1,9 @@
-﻿using System.Diagnostics;
-
-namespace TestHelper
+﻿namespace TestHelper
 {
     using System;
     using System.Collections.Generic;
     using System.Collections.Immutable;
+    using System.Diagnostics;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
