5ddb6627197cf6013d28df178cc20885d5a947f1
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-05 12:20:38 -0700

    Split CallbackSignatureShouldMatchMockedMethod tests up per-test case

--- Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodAnalyzerTests.cs
+++ Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodAnalyzerTests.cs
@@ -6,12 +6,37 @@ using Xunit;
 
 namespace Moq.Analyzers.Test;
 
+// TODO: These tests should be broken down further
 public class CallbackSignatureShouldMatchMockedMethodAnalyzerTests : DiagnosticVerifier
 {
     [Fact]
-    public Task ShouldPassIfGoodParameters()
+    public Task ShouldPassWhenCorrectSetupAndReturns()
     {
-        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.cs")));
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.MyGoodSetupAndReturns.cs")));
+    }
+
+    [Fact]
+    public Task ShouldFailWhenIncorrectCallbacks()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestBadCallbacks.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndCallbacks()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndCallback.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndParameterlessCallbacks()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndParameterlessCallback.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndReturnsAndCallbacks()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndReturnsAndCallback.cs")));
     }
 
     protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
--- Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodCodeFixTests.cs
+++ Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodCodeFixTests.cs
@@ -10,9 +10,33 @@ namespace Moq.Analyzers.Test;
 public class CallbackSignatureShouldMatchMockedMethodCodeFixTests : CodeFixVerifier
 {
     [Fact]
-    public Task ShouldSuggestQuickFixIfBadParameters()
+    public Task ShouldPassWhenCorrectSetupAndReturns()
     {
-        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.cs")));
+        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.MyGoodSetupAndReturns.cs")));
+    }
+
+    [Fact]
+    public Task ShouldSuggestQuickFixWhenIncorrectCallbacks()
+    {
+        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestBadCallbacks.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndCallbacks()
+    {
+        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndCallback.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndParameterlessCallbacks()
+    {
+        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndParameterlessCallback.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenCorrectSetupAndReturnsAndCallbacks()
+    {
+        return Verify(VerifyCSharpFix(File.ReadAllText("Data/CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndReturnsAndCallback.cs")));
     }
 
     protected override CodeFixProvider GetCSharpCodeFixProvider()
