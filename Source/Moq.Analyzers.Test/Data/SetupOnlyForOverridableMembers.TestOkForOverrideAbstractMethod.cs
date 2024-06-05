d020985dccdcb0af3499aeb437734a4d4177ebe6
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-05 14:54:57 -0700

    Split SetupOnlyForOverridableMembers tests

---
+++ Source/Moq.Analyzers.Test/Data/SetupOnlyForOverridableMembers.TestOkForOverrideAbstractMethod.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+
+namespace SetupOnlyForOverridableMembers.TestOkForOverrideAbstractMethod;
+
+public interface ISampleInterface
+{
+    int Calculate(int a, int b);
+
+    int TestProperty { get; set; }
+}
+
+public abstract class BaseSampleClass
+{
+    public int Calculate() => 0;
+
+    public abstract int Calculate(int a, int b);
+
+    public abstract int Calculate(int a, int b, int c);
+}
+
+public class SampleClass : BaseSampleClass
+{
+
+    public override int Calculate(int a, int b) => 0;
+
+    public sealed override int Calculate(int a, int b, int c) => 0;
+
+    public virtual int DoSth() => 0;
+
+    public int Property { get; set; }
+}
+
+internal class MyUnitTests
+{
+    private void TestOkForOverrideAbstractMethod()
+    {
+        var mock = new Mock<SampleClass>();
+        mock.Setup(x => x.Calculate(It.IsAny<int>(), It.IsAny<int>()));
+    }
+}
--- Source/Moq.Analyzers.Test/SetupShouldBeUsedOnlyForOverridableMembersAnalyzerTests.cs
+++ Source/Moq.Analyzers.Test/SetupShouldBeUsedOnlyForOverridableMembersAnalyzerTests.cs
@@ -9,9 +9,51 @@ namespace Moq.Analyzers.Test;
 public class SetupShouldBeUsedOnlyForOverridableMembersAnalyzerTests : DiagnosticVerifier
 {
     [Fact]
-    public Task ShouldPassIfGoodParameters()
+    public Task ShouldFailWhenSetupIsCalledWithANonVirtualMethod()
     {
-        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.cs")));
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestBadSetupForNonVirtualMethod.cs")));
+    }
+
+    [Fact]
+    public Task ShouldFailWhenSetupIsCalledWithANonVirtualProperty()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestBadSetupForNonVirtualProperty.cs")));
+    }
+
+    [Fact]
+    public Task ShouldFailWhenSetupIsCalledWithASealedMethod()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestBadSetupForSealedMethod.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenSetupIsCalledWithAnAbstractMethod()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestOkForAbstractMethod.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenSetupIsCalledWithAnInterfaceMethod()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestOkForInterfaceMethod.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenSetupIsCalledWithAnInterfaceProperty()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestOkForInterfaceProperty.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenSetupIsCalledWithAnOverrideOfAnAbstractMethod()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestOkForOverrideAbstractMethod.cs")));
+    }
+
+    [Fact]
+    public Task ShouldPassWhenSetupIsCalledWithAVirtualMethod()
+    {
+        return Verify(VerifyCSharpDiagnostic(File.ReadAllText("Data/SetupOnlyForOverridableMembers.TestOkForVirtualMethod.cs")));
     }
 
     protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
