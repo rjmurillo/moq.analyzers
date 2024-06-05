f2922b668c86b376ae9cd072fc83ddabd4a29421
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-05 14:31:25 -0700

    Simplify names for NoSealedClassMocks

--- Source/Moq.Analyzers.Test/Data/NoSealedClassMocks.cs
+++ Source/Moq.Analyzers.Test/Data/NoSealedClassMocks.cs
@@ -3,24 +3,19 @@ using Moq;
 
 namespace NoSealedClassMocks;
 
-internal sealed class FooSealed
-{
-    private void Do(string s) { }
-}
+internal sealed class FooSealed { }
+
+internal class Foo { }
 
 internal class MyUnitTests
 {
     private void Test()
     {
-        var mock1 = new Moq.Mock<FooSealed>();
-        var mock2 = new Mock<FooSealed>();
-        var mock3 = new Mock<NoSealedClassMocks.FooSealed>();
-        var mock4 = new Moq.Mock<NoSealedClassMocks.FooSealed>();
+        var mock = new Mock<FooSealed>();
     }
 
     private void Test2()
     {
-        new Mock<Action<int>>();
-        new Mock<EventHandler>();
+        var mock = new Mock<Foo>();
     }
 }
