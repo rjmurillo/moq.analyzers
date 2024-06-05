b1a0819de8ff362c43bfb55056c73043a6110464
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-05 13:21:40 -0700

    Simplify names for ConstructorArgumentsShouldMatch tests

--- Source/Moq.Analyzers.Test/Data/ConstructorArgumentsShouldMatch.cs
+++ Source/Moq.Analyzers.Test/Data/ConstructorArgumentsShouldMatch.cs
@@ -19,33 +19,33 @@ internal class MyUnitTests
 {
     private void TestBad()
     {
-        var mock1 = new Moq.Mock<Foo>(1, true);
-        var mock2 = new Mock<ConstructorArgumentsShouldMatch.Foo>(2, true);
-        var mock3 = new Mock<ConstructorArgumentsShouldMatch.Foo>("1", 3);
-        var mock4 = new Mock<ConstructorArgumentsShouldMatch.Foo>(new int[] { 1, 2, 3 });
+        var mock1 = new Mock<Foo>(1, true);
+        var mock2 = new Mock<Foo>(2, true);
+        var mock3 = new Mock<Foo>("1", 3);
+        var mock4 = new Mock<Foo>(new int[] { 1, 2, 3 });
     }
 
-    private void TestBad2()
+    private void TestBadWithMockBehavior()
     {
         var mock1 = new Mock<Foo>(MockBehavior.Strict, 4, true);
-        var mock2 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>(MockBehavior.Loose, 5, true);
-        var mock3 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>(MockBehavior.Loose, "2", 6);
+        var mock2 = new Mock<Foo>(MockBehavior.Loose, 5, true);
+        var mock3 = new Mock<Foo>(MockBehavior.Loose, "2", 6);
     }
 
-    private void TestGood1()
+    private void TestGood()
     {
-        var mock1 = new Moq.Mock<Foo>(MockBehavior.Default);
+        var mock1 = new Mock<Foo>(MockBehavior.Default);
         var mock2 = new Mock<Foo>(MockBehavior.Strict);
-        var mock3 = new Mock<ConstructorArgumentsShouldMatch.Foo>(MockBehavior.Loose);
-        var mock4 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>(MockBehavior.Default);
+        var mock3 = new Mock<Foo>(MockBehavior.Loose);
+        var mock4 = new Mock<Foo>(MockBehavior.Default);
 
         var mock5 = new Mock<Foo>("3");
-        var mock6 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>("4");
-        var mock7 = new Moq.Mock<Foo>(Moq.MockBehavior.Default, "5");
-        var mock8 = new Mock<ConstructorArgumentsShouldMatch.Foo>(Moq.MockBehavior.Default, "6");
+        var mock6 = new Mock<Foo>("4");
+        var mock7 = new Mock<Foo>(MockBehavior.Default, "5");
+        var mock8 = new Mock<Foo>(MockBehavior.Default, "6");
 
-        var mock9 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>(false, 0);
-        var mock10 = new Moq.Mock<ConstructorArgumentsShouldMatch.Foo>(Moq.MockBehavior.Default, true, 1);
+        var mock9 = new Mock<Foo>(false, 0);
+        var mock10 = new Mock<Foo>(MockBehavior.Default, true, 1);
 
         var mock11 = new Mock<Foo>(DateTime.Now, DateTime.Now);
         var mock12 = new Mock<Foo>(MockBehavior.Default, DateTime.Now, DateTime.Now);
