d40c37b794ef0fee52e9135fb021f38cbcd56f15
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-06 13:17:54 -0700

    Convert CallbackSignatureShouldMatchMockedMethodCodeFix tests (and merge with analyzer tests

--- Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodCodeFixTests.cs
+++ Source/Moq.Analyzers.Test/CallbackSignatureShouldMatchMockedMethodCodeFixTests.cs
@@ -1,40 +1,314 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Moq.Analyzers.Test.Helpers;
 using Xunit;
 
 namespace Moq.Analyzers.Test;
 
-//public class CallbackSignatureShouldMatchMockedMethodCodeFixTests : CallbackSignatureShouldMatchMockedMethodBase
-//{
-//    // [Fact]
-//    public Task ShouldPassWhenCorrectSetupAndReturns()
-//    {
-//        return Verify(VerifyCSharpFix(GoodSetupAndReturns));
-//    }
+public class CallbackSignatureShouldMatchMockedMethodCodeFixTests : CodeFixVerifier<CallbackSignatureShouldMatchMockedMethodAnalyzer, CallbackSignatureShouldMatchMockedMethodCodeFix>
+{
+    [Fact]
+    public async Task ShouldPassWhenCorrectSetupAndReturns()
+    {
+        await VerifyCSharpFix(
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
 
-//    // [Fact]
-//    public Task ShouldSuggestQuickFixWhenIncorrectCallbacks()
-//    {
-//        return Verify(VerifyCSharpFix(BadCallbacks));
-//    }
+            namespace CallbackSignatureShouldMatchMockedMethod.MyGoodSetupAndReturns;
 
-//    // [Fact]
-//    public Task ShouldPassWhenCorrectSetupAndCallbacks()
-//    {
-//        return Verify(VerifyCSharpFix(GoodSetupAndCallback));
-//    }
+            internal interface IFoo
+            {
+                int Do(string s);
 
-//    // [Fact]
-//    public Task ShouldPassWhenCorrectSetupAndParameterlessCallbacks()
-//    {
-//        return Verify(VerifyCSharpFix(GoodSetupAndParameterlessCallback));
-//    }
+                int Do(int i, string s, DateTime dt);
 
-//    // [Fact]
-//    public Task ShouldPassWhenCorrectSetupAndReturnsAndCallbacks()
-//    {
-//        return Verify(VerifyCSharpFix(GoodSetupAndReturnsAndCallback));
-//    }
-//}
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void MyGoodSetupAndReturns()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Returns((string s) => { return 0; });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Returns((int i, string s, DateTime dt) => { return 0; });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Returns((List<string> l) => { return 0; });
+                }
+            }
+            """,
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.MyGoodSetupAndReturns;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void MyGoodSetupAndReturns()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Returns((string s) => { return 0; });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Returns((int i, string s, DateTime dt) => { return 0; });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Returns((List<string> l) => { return 0; });
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task ShouldSuggestQuickFixWhenIncorrectCallbacks()
+    {
+        await VerifyCSharpFix(
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestBadCallbacks;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestBadCallbacks()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback({|Moq1100:(int i)|} => { });
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback({|Moq1100:(string s1, string s2)|} => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback({|Moq1100:(string s1, int i1)|} => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback({|Moq1100:(int i)|} => { });
+                }
+            }
+            """,
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestBadCallbacks;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestBadCallbacks()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback((int i, string s, DateTime dt) => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback((List<string> l) => { });
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task ShouldPassWhenCorrectSetupAndCallbacks()
+    {
+        await VerifyCSharpFix(
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback((int i, string s, DateTime dt) => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback((List<string> l) => { });
+                }
+            }
+            """,
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback((int i, string s, DateTime dt) => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback((List<string> l) => { });
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task ShouldPassWhenCorrectSetupAndParameterlessCallbacks()
+    {
+        await VerifyCSharpFix(
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndParameterlessCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndParameterlessCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback(() => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback(() => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback(() => { });
+                }
+            }
+            """,
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndParameterlessCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndParameterlessCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Callback(() => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Callback(() => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Callback(() => { });
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task ShouldPassWhenCorrectSetupAndReturnsAndCallbacks()
+    {
+        await VerifyCSharpFix(
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndReturnsAndCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndReturnsAndCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Returns(0).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Returns(0).Callback((int i, string s, DateTime dt) => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Returns(0).Callback((List<string> l) => { });
+                }
+            }
+            """,
+            """
+            using System;
+            using System.Collections.Generic;
+            using Moq;
+
+            namespace CallbackSignatureShouldMatchMockedMethod.TestGoodSetupAndReturnsAndCallback;
+
+            internal interface IFoo
+            {
+                int Do(string s);
+
+                int Do(int i, string s, DateTime dt);
+
+                int Do(List<string> l);
+            }
+
+            internal class MyUnitTests
+            {
+                private void TestGoodSetupAndReturnsAndCallback()
+                {
+                    var mock = new Mock<IFoo>();
+                    mock.Setup(x => x.Do(It.IsAny<string>())).Returns(0).Callback((string s) => { });
+                    mock.Setup(x => x.Do(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<DateTime>())).Returns(0).Callback((int i, string s, DateTime dt) => { });
+                    mock.Setup(x => x.Do(It.IsAny<List<string>>())).Returns(0).Callback((List<string> l) => { });
+                }
+            }
+            """);
+    }
+}
--- Source/Moq.Analyzers.Test/Helpers/CodeFixVerifier.cs
+++ Source/Moq.Analyzers.Test/Helpers/CodeFixVerifier.cs
@@ -1,28 +1,24 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing;
 
 namespace Moq.Analyzers.Test.Helpers;
 
-public abstract class DiagnosticVerifier<TAnalyzer>
+public abstract class CodeFixVerifier<TAnalyzer, TCodeFixProvider>
     where TAnalyzer : DiagnosticAnalyzer, new()
+    where TCodeFixProvider : CodeFixProvider, new()
 {
-    protected async Task VerifyCSharpDiagnostic(string source)
+    protected async Task VerifyCSharpFix(string originalSource, string fixedSource)
     {
-        CSharpAnalyzerTest<TAnalyzer, DefaultVerifier> context = new()
+        CSharpCodeFixTest<TAnalyzer, TCodeFixProvider, DefaultVerifier> context = new()
         {
             // TODO: Refactor this out
             ReferenceAssemblies = ReferenceAssemblies.Net.Net80.AddPackages([new PackageIdentity("Moq", "4.8.2")]), // TODO: See https://github.com/Litee/moq.analyzers/issues/58
             TestBehaviors = TestBehaviors.SkipGeneratedCodeCheck, // TODO: We should enable the generated code check
-            TestCode = source,
+            TestCode = originalSource,
+            FixedCode = fixedSource,
         };
-
-        context.SolutionTransforms.Add((solution, projectId) =>
-        {
-            return solution.WithProjectParseOptions(projectId, new CSharpParseOptions(
-                languageVersion: LanguageVersion.Latest));
-        });
 
         await context.RunAsync().ConfigureAwait(false);
     }
