66837a746783e27b1e3df9791d28a00b31cb0336
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-08 15:26:11 -0700

    Use Theory data in AsAcceptOnlyInterface tests

--- Source/Moq.Analyzers.Test/AsAcceptOnlyInterfaceAnalyzerTests.cs
+++ Source/Moq.Analyzers.Test/AsAcceptOnlyInterfaceAnalyzerTests.cs
@@ -4,35 +4,26 @@ namespace Moq.Analyzers.Test;
 
 public class AsAcceptOnlyInterfaceAnalyzerTests
 {
-    [Fact]
-    public async Task ShouldFailWhenUsingAsWithAbstractClass()
+    public static IEnumerable<object[]> TestData()
     {
-        await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace AsAcceptOnlyInterface.TestBadAsForAbstractClass;
-
-                public abstract class BaseSampleClass
-                {
-                    public int Calculate() => 0;
-                }
-
-                internal class MyUnitTests
-                {
-                    private void TestBadAsForAbstractClass()
-                    {
-                        var mock = new Mock<BaseSampleClass>();
-                        mock.As<{|Moq1300:BaseSampleClass|}>();
-                    }
-                }
-                """);
+        foreach (var @namespace in new[] { string.Empty, "namespace MyNamespace;" })
+        {
+            // TODO: .As<BaseSampleClass> and .As<SampleClass> feels redundant
+            yield return [@namespace, """new Mock<BaseSampleClass>().As<{|Moq1300:BaseSampleClass|}>();"""];
+            yield return [@namespace, """new Mock<BaseSampleClass>().As<{|Moq1300:SampleClass|}>();"""];
+            yield return [@namespace, """new Mock<SampleClass>().As<ISampleInterface>();"""];
+            // TODO: Testing with .Setup() and .Returns() seems unnecessary.
+            yield return [@namespace, """new Mock<SampleClass>().As<ISampleInterface>().Setup(x => x.Calculate(It.IsAny<int>(), It.IsAny<int>())).Returns(10);"""];
+        }
     }
 
-    [Fact]
-    public async Task ShouldFailWhenUsingAsWithConcreteClass()
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task ShouldAnalyzeAs(string @namespace, string mock)
     {
         await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace AsAcceptOnlyInterface.TestBadAsForNonAbstractClass;
+                $$"""
+                {{@namespace}}
 
                 public interface ISampleInterface
                 {
@@ -44,76 +35,17 @@ public class AsAcceptOnlyInterfaceAnalyzerTests
                     public int Calculate() => 0;
                 }
 
-                public class OtherClass
-                {
-
-                    public int Calculate() => 0;
-                }
-
-                internal class MyUnitTests
-                {
-                    private void TestBadAsForNonAbstractClass()
-                    {
-                        var mock = new Mock<BaseSampleClass>();
-                        mock.As<{|Moq1300:OtherClass|}>();
-                    }
-                }
-                """);
-    }
-
-    [Fact]
-    public async Task ShouldPassWhenUsingAsWithInterface()
-    {
-        await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace AsAcceptOnlyInterface.TestOkAsForInterface;
-
-                public interface ISampleInterface
-                {
-                    int Calculate(int a, int b);
-                }
-
                 public class SampleClass
                 {
+
                     public int Calculate() => 0;
                 }
 
-                internal class MyUnitTests
+                internal class UnitTest
                 {
-                    private void TestOkAsForInterface()
+                    private void Test()
                     {
-                        var mock = new Mock<SampleClass>();
-                        mock.As<ISampleInterface>();
-                    }
-                }
-                """);
-    }
-
-    [Fact]
-    public async Task ShouldPassWhenUsingAsWithInterfaceWithSetup()
-    {
-        await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace AsAcceptOnlyInterface.TestOkAsForInterfaceWithConfiguration;
-
-                public interface ISampleInterface
-                {
-                    int Calculate(int a, int b);
-                }
-
-                public class SampleClass
-                {
-                    public int Calculate() => 0;
-                }
-
-                internal class MyUnitTests
-                {
-                    private void TestOkAsForInterfaceWithConfiguration()
-                    {
-                        var mock = new Mock<SampleClass>();
-                        mock.As<ISampleInterface>()
-                            .Setup(x => x.Calculate(It.IsAny<int>(), It.IsAny<int>()))
-                            .Returns(10);
+                        {{mock}}
                     }
                 }
                 """);
