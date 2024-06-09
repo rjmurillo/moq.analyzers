339cc259c6fb099ca3a314750037ef5bbb1fbc79
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-08 21:57:04 -0700

    Use Theory data in NoConstructorArgumentsForInterfaceMockAnalyzer tests

--- Source/Moq.Analyzers.Test/NoConstructorArgumentsForInterfaceMockAnalyzerTests.cs
+++ Source/Moq.Analyzers.Test/NoConstructorArgumentsForInterfaceMockAnalyzerTests.cs
@@ -4,82 +4,49 @@ namespace Moq.Analyzers.Test;
 
 public class NoConstructorArgumentsForInterfaceMockAnalyzerTests
 {
-    [Fact]
-    public async Task ShouldFailIfMockedInterfaceHasConstructorParameters()
+    public static IEnumerable<object[]> InterfaceMockingTestData()
+    {
+        foreach (string @namespace in new[] { string.Empty, "namespace MyNamespace;" })
+        {
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(25, true)|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:("123")|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(25, true)|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:("123")|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(MockBehavior.Default, "123")|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(MockBehavior.Strict, 25, true)|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(MockBehavior.Default, "123")|};"""];
+            yield return [@namespace, """new Mock<IMyService>{|Moq1001:(MockBehavior.Loose, 25, true)|};"""];
+            yield return [@namespace, """new Mock<IMyService>();"""];
+            yield return [@namespace, """new Mock<IMyService>(MockBehavior.Default);"""];
+            yield return [@namespace, """new Mock<IMyService>(MockBehavior.Strict);"""];
+            yield return [@namespace, """new Mock<IMyService>(MockBehavior.Loose);"""];
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(InterfaceMockingTestData))]
+    public async Task ShouldAnalyzeInterfaceConstructors(string @namespace, string mock)
     {
         await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace NoConstructorArgumentsForInterfaceMock.TestBad;
+                $$"""
+                {{@namespace}}
 
                 internal interface IMyService
                 {
                     void Do(string s);
                 }
 
-                internal class MyUnitTests
+                internal class UnitTest
                 {
-                    private void TestBad()
+                    private void Test()
                     {
-                        var mock1 = new Mock<IMyService>{|Moq1001:(25, true)|};
-                        var mock2 = new Mock<IMyService>{|Moq1001:("123")|};
-                        var mock3 = new Mock<IMyService>{|Moq1001:(25, true)|};
-                        var mock4 = new Mock<IMyService>{|Moq1001:("123")|};
+                        {{mock}}
                     }
                 }
                 """);
     }
 
-    [Fact]
-    public async Task ShouldFailIfMockedInterfaceHasConstructorParametersAndExplicitMockBehavior()
-    {
-        await Verifier.VerifyAnalyzerAsync(
-                """
-                namespace NoConstructorArgumentsForInterfaceMock.TestBadWithMockBehavior;
-
-                internal interface IMyService
-                {
-                    void Do(string s);
-                }
-
-                internal class MyUnitTests
-                {
-                    private void TestBadWithMockBehavior()
-                    {
-                        var mock1 = new Mock<IMyService>{|Moq1001:(MockBehavior.Default, "123")|};
-                        var mock2 = new Mock<IMyService>{|Moq1001:(MockBehavior.Strict, 25, true)|};
-                        var mock3 = new Mock<IMyService>{|Moq1001:(MockBehavior.Default, "123")|};
-                        var mock4 = new Mock<IMyService>{|Moq1001:(MockBehavior.Loose, 25, true)|};
-                    }
-                }
-                """);
-    }
-
-    [Fact]
-    public async Task ShouldPassIfMockedInterfaceDoesNotHaveConstructorParameters()
-    {
-        await Verifier.VerifyAnalyzerAsync(
-            """
-            namespace NoConstructorArgumentsForInterfaceMock.TestGood;
-
-            internal interface IMyService
-            {
-                void Do(string s);
-            }
-
-            internal class MyUnitTests
-            {
-                private void TestGood()
-                {
-                    var mock1 = new Mock<IMyService>();
-                    var mock2 = new Mock<IMyService>(MockBehavior.Default);
-                    var mock3 = new Mock<IMyService>(MockBehavior.Strict);
-                    var mock4 = new Mock<IMyService>(MockBehavior.Loose);
-                }
-            }
-            """);
-    }
-
-    // TODO: This feels like it should be in every analyzer's tests
+    // TODO: This feels like it should be in every analyzer's tests. Tracked by #75.
     [Fact]
     public async Task ShouldPassIfCustomMockClassIsUsed()
     {
@@ -126,7 +93,7 @@ public class NoConstructorArgumentsForInterfaceMockAnalyzerTests
                 """);
     }
 
-    // TODO: This feels duplicated with other tests
+    // TODO: This feels like it should be in every analyzer's tests. Tracked by #75.
     [Fact]
     public async Task ShouldFailIsRealMoqIsUsedWithInvalidParameters()
     {
@@ -173,6 +140,7 @@ public class NoConstructorArgumentsForInterfaceMockAnalyzerTests
                 """);
     }
 
+    // TODO: This feels like it should be in every analyzer's tests. Tracked by #75.
     [Fact]
     public async Task ShouldPassIfRealMoqIsUsedWithValidParameters()
     {
