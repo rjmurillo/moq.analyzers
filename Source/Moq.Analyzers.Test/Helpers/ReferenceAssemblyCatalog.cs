8fbb589223502eae1b0564c66bcc0e3a292fdc33
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-12 00:25:24 -0700

    Add comment to document why Moq test versions were picked

--- Source/Moq.Analyzers.Test/Helpers/ReferenceAssemblyCatalog.cs
+++ Source/Moq.Analyzers.Test/Helpers/ReferenceAssemblyCatalog.cs
@@ -20,7 +20,11 @@ internal static class ReferenceAssemblyCatalog
 
     public static IReadOnlyDictionary<string, ReferenceAssemblies> Catalog { get; } = new Dictionary<string, ReferenceAssemblies>(StringComparer.Ordinal)
     {
+        // 4.8.2 was one of the first popular versions of Moq. Ensure this version is prior to 4.13.1, as it changed the internal
+        // implementation of `.As<T>()` (see https://github.com/devlooped/moq/commit/b552aeddd82090ee0f4743a1ab70a16f3e6d2d11).
         { nameof(Net80WithOldMoq), ReferenceAssemblies.Net.Net80.AddPackages([new PackageIdentity("Moq", "4.8.2")]) },
+
+        // 4.18.4 is currently the most downloaded version of Moq.
         { nameof(Net80WithNewMoq), ReferenceAssemblies.Net.Net80.AddPackages([new PackageIdentity("Moq", "4.18.4")]) },
     };
 }
