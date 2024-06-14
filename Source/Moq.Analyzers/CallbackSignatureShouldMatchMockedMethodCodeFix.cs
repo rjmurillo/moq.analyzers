5c09a3643f949705e4d0167918f1048f369d1613
Richard Murillo <richard.murillo@microsoft.com>
2024-06-14 11:44:05 -0700

    Add suppressions for AV1500

--- Source/Moq.Analyzers/CallbackSignatureShouldMatchMockedMethodCodeFix.cs
+++ Source/Moq.Analyzers/CallbackSignatureShouldMatchMockedMethodCodeFix.cs
@@ -54,6 +54,7 @@ public class CallbackSignatureShouldMatchMockedMethodCodeFix : CodeFixProvider
             diagnostic);
     }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1500:Member or local function contains too many statements", Justification = "Tracked in https://github.com/rjmurillo/moq.analyzers/issues/90")]
     private async Task<Document> FixCallbackSignatureAsync(SyntaxNode root, Document document, ParameterListSyntax? oldParameters, CancellationToken cancellationToken)
     {
         SemanticModel? semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
