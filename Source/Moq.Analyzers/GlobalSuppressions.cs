9b6f01cedf390721c120b6c8e4d81c1a829ed88d
Matt Kotsenas <Matt.Kotsenas@gmail.com>
2024-06-14 09:05:27 -0700

    Fix trivial warnings (#88)
    Contributes to #26 (more work needed).
    
    Fix trivial cases of warnings and move some duplicated analyzer settings
    from `GlobalSupressions.cs` to `.editorconfig`.

--- Source/Moq.Analyzers/GlobalSuppressions.cs
+++ Source/Moq.Analyzers/GlobalSuppressions.cs
@@ -3,8 +3,4 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Not required")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Not required")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces must not be omitted", Justification = "Not required")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Not required")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Not required")]
