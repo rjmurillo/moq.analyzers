d05283ee8bdf147e7909b1927d46b4b9f7ead819
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-06-21 10:22:51 -0700

    cleanup

---
+++ Source/Moq.Analyzers.Benchmarks/Helpers/AnalysisResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Moq.Analyzers.Benchmarks.Helpers;
+
+internal static class AnalysisResultExtensions
+{
+    public static AnalysisResult AssertValidAnalysisResult(this AnalysisResult analysisResult)
+    {
+        if (analysisResult.Analyzers.Length != 1)
+        {
+            throw new InvalidOperationException($"Expected a single analyzer but found '{analysisResult.Analyzers.Length}'");
+        }
+
+        if (analysisResult.CompilationDiagnostics.Count != 0)
+        {
+            throw new InvalidOperationException($"Expected no compilation diagnostics but found '{analysisResult.CompilationDiagnostics.Count}'");
+        }
+
+        return analysisResult;
+    }
+}
---
+++ Source/Moq.Analyzers.Benchmarks/Moq1300Benchmarks.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Immutable;
+using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Moq.Analyzers.Benchmarks.Helpers;
+
+namespace Moq.Analyzers.Benchmarks;
+
+[InProcess]
+[MemoryDiagnoser]
+public class Moq1300Benchmarks
+{
+    private static CompilationWithAnalyzers? BaselineCompilation { get; set; }
+
+    private static CompilationWithAnalyzers? TestCompilation { get; set; }
+
+    [IterationSetup]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "Async setup not supported in BenchmarkDotNet.See https://github.com/dotnet/BenchmarkDotNet/issues/2442.")]
+    public static void SetupCompilation()
+    {
+        List<(string Name, string Content)> sources = [];
+        for (int i = 0; i < Constants.NumberOfCodeFiles; i++)
+        {
+            string name = "TypeName" + i;
+            sources.Add((name, @$"
+using System;
+using Moq;
+
+public class SampleClass{i}
+{{
+
+    public int Calculate() => 0;
+}}
+
+internal class {name}
+{{
+    private void Test()
+    {{
+        new Mock<SampleClass{i}>().As<SampleClass{i}>();
+    }}
+}}
+"));
+        }
+
+        (BaselineCompilation, TestCompilation) =
+            BenchmarkCSharpCompilationCreator<AsShouldBeUsedOnlyForInterfaceAnalyzer>
+            .Create(sources.ToArray())
+            .GetAwaiter()
+            .GetResult();
+    }
+
+    [Benchmark]
+    public async Task Moq1300WithDiagnostics()
+    {
+        ImmutableArray<Diagnostic> diagnostics =
+            (await TestCompilation!
+            .GetAnalysisResultAsync(CancellationToken.None)
+            .ConfigureAwait(false))
+            .AssertValidAnalysisResult()
+            .GetAllDiagnostics();
+
+        if (diagnostics.Length != Constants.NumberOfCodeFiles)
+        {
+            throw new InvalidOperationException($"Expected '{Constants.NumberOfCodeFiles:N0}' analyzer diagnostics but found '{diagnostics.Length}'");
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public async Task Moq1300Baseline()
+    {
+        ImmutableArray<Diagnostic> diagnostics =
+            (await BaselineCompilation!
+            .GetAnalysisResultAsync(CancellationToken.None)
+            .ConfigureAwait(false))
+            .AssertValidAnalysisResult()
+            .GetAllDiagnostics();
+
+        if (diagnostics.Length != 0)
+        {
+            throw new InvalidOperationException($"Expected no analyzer diagnostics but found '{diagnostics.Length}'");
+        }
+    }
+}
