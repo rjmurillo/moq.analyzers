16024323fdc4af529a2c04904baba0b122cee007
Richard Murillo <richard.murillo@microsoft.com>
2024-07-05 16:56:02 -0700

    Revert cases for internal and protected internal

--- tests/Moq.Analyzers.Test/ConstructorArgumentsShouldMatchAnalyzerTests.PrivateCtors.cs
+++ tests/Moq.Analyzers.Test/ConstructorArgumentsShouldMatchAnalyzerTests.PrivateCtors.cs
@@ -1,6 +1,7 @@
 ﻿using Verifier = Moq.Analyzers.Test.Helpers.AnalyzerVerifier<Moq.Analyzers.ConstructorArgumentsShouldMatchAnalyzer>;
 
 namespace Moq.Analyzers.Test;
+
 public partial class ConstructorArgumentsShouldMatchAnalyzerTests
 {
     // This is to avoid a Castle System.ArgumentException at runtime
@@ -10,12 +11,15 @@ public partial class ConstructorArgumentsShouldMatchAnalyzerTests
     {
         IEnumerable<object[]> all = new object[][]
         {
+            // Traditional
             ["""new Mock<NoConstructorClass>{|Moq1002:(MockBehavior.Default)|};"""],
             ["""new Mock<NoConstructorClass>{|Moq1002:()|};"""],
 
+            // LINQ
             ["""{|Moq1002:Mock.Of<NoConstructorClass>()|};"""],
             ["""{|Moq1002:Mock.Of<NoConstructorClass>(m => true)|};"""],
 
+            // Repository
             ["""var repository = new MockRepository(MockBehavior.Default) { DefaultValue = DefaultValue.Empty }; var fooMock = repository.Create<NoConstructorClass>{|Moq1002:(MockBehavior.Default)|}; repository.Verify();"""],
             ["""var repository = new MockRepository(MockBehavior.Default) { DefaultValue = DefaultValue.Empty }; var fooMock = repository.Create<NoConstructorClass>{|Moq1002:()|}; repository.Verify();"""],
         }.WithNamespaces().WithMoqReferenceAssemblyGroups();
@@ -40,54 +44,6 @@ public partial class ConstructorArgumentsShouldMatchAnalyzerTests
               public class NoConstructorClass
               {
                 private NoConstructorClass() { }
-              }
-
-              internal class UnitTest
-              {
-                  private void Test()
-                  {
-                      {{mock}}
-                  }
-              }
-              """,
-            referenceAssemblyGroup);
-    }
-
-    [Theory]
-    [MemberData(nameof(ClassWithNoCtorTestData))]
-    public async Task ShouldAnalyzeClassWithInternalCtors(string referenceAssemblyGroup, string @namespace, string mock)
-    {
-        await Verifier.VerifyAnalyzerAsync(
-            $$"""
-              {{@namespace}}
-
-              public class NoConstructorClass
-              {
-                internal NoConstructorClass() { }
-              }
-
-              internal class UnitTest
-              {
-                  private void Test()
-                  {
-                      {{mock}}
-                  }
-              }
-              """,
-            referenceAssemblyGroup);
-    }
-
-    [Theory]
-    [MemberData(nameof(ClassWithNoCtorTestData))]
-    public async Task ShouldAnalyzeClassWithProtectedInternalCtors(string referenceAssemblyGroup, string @namespace, string mock)
-    {
-        await Verifier.VerifyAnalyzerAsync(
-            $$"""
-              {{@namespace}}
-
-              public abstract class NoConstructorClass
-              {
-                protected internal NoConstructorClass() { }
               }
 
               internal class UnitTest
