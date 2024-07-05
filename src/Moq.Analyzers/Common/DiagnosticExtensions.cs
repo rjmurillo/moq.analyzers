4fb7c5b4e82b099f735a54b1f5b24346ba9b1273
Richard Murillo <richard.murillo@microsoft.com>
2024-07-05 15:52:29 -0700

    Remove unused code

--- src/Moq.Analyzers/Common/DiagnosticExtensions.cs
+++ src/Moq.Analyzers/Common/DiagnosticExtensions.cs
@@ -30,56 +30,6 @@ internal static class DiagnosticExtensions
                 messageArgs: messageArgs);
 
     public static Diagnostic CreateDiagnostic(
-            this IOperation operation,
-            DiagnosticDescriptor rule,
-            params object?[]? messageArgs)
-            => operation.CreateDiagnostic(rule, properties: null, messageArgs);
-
-    public static Diagnostic CreateDiagnostic(
-        this IOperation operation,
-        DiagnosticDescriptor rule,
-        ImmutableDictionary<string, string?>? properties,
-        params object?[]? messageArgs)
-    {
-        return operation.Syntax.CreateDiagnostic(rule, properties, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this IOperation operation,
-        DiagnosticDescriptor rule,
-        ImmutableArray<Location> additionalLocations,
-        ImmutableDictionary<string, string?>? properties,
-        params object?[]? messageArgs)
-    {
-        return operation.Syntax.CreateDiagnostic(rule, additionalLocations, properties, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this SyntaxToken token,
-        DiagnosticDescriptor rule,
-        params object?[]? messageArgs)
-    {
-        return token.GetLocation().CreateDiagnostic(rule, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this ISymbol symbol,
-        DiagnosticDescriptor rule,
-        params object?[]? messageArgs)
-    {
-        return symbol.Locations.CreateDiagnostic(rule, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this ISymbol symbol,
-        DiagnosticDescriptor rule,
-        ImmutableDictionary<string, string?>? properties,
-        params object?[]? messageArgs)
-    {
-        return symbol.Locations.CreateDiagnostic(rule, properties, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
         this Location location,
         DiagnosticDescriptor rule,
         params object?[]? messageArgs)
@@ -114,33 +64,5 @@ internal static class DiagnosticExtensions
             additionalLocations: additionalLocations,
             properties: properties,
             messageArgs: messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this IEnumerable<Location> locations,
-        DiagnosticDescriptor rule,
-        params object?[]? messageArgs)
-    {
-        return locations.CreateDiagnostic(rule, properties: null, messageArgs);
-    }
-
-    public static Diagnostic CreateDiagnostic(
-        this IEnumerable<Location> locations,
-        DiagnosticDescriptor rule,
-        ImmutableDictionary<string, string?>? properties,
-        params object?[]? messageArgs)
-    {
-        IEnumerable<Location> inSource = locations.Where(location => location.IsInSource);
-        if (!inSource.Any())
-        {
-            return Diagnostic.Create(rule, location: null, messageArgs);
-        }
-
-        return Diagnostic.Create(
-                 rule,
-                 location: inSource.First(),
-                 additionalLocations: inSource.Skip(1),
-                 properties: properties,
-                 messageArgs: messageArgs);
     }
 }
