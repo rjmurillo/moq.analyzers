20302fe4b64b43cbe52aa7276f06e39df26b39f7
Richard Murillo <rjmurillo@gmail.com>
2024-07-06 13:24:57 -0700

    Add version detection logic
    Analyzer 1201 should only be firing on versions of Moq prior to 4.16
    
    Fixes #144

--- src/Moq.Analyzers/SetupShouldNotIncludeAsyncResultAnalyzer.cs
+++ src/Moq.Analyzers/SetupShouldNotIncludeAsyncResultAnalyzer.cs
@@ -33,6 +33,22 @@ public class SetupShouldNotIncludeAsyncResultAnalyzer : DiagnosticAnalyzer
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1500:Member or local function contains too many statements", Justification = "Tracked in https://github.com/rjmurillo/moq.analyzers/issues/90")]
     private static void Analyze(SyntaxNodeAnalysisContext context)
     {
+        // This rule only applies to versions of Moq earlier than 4.16
+        ImmutableArray<INamedTypeSymbol> mockTypes = context.Compilation.GetMoqMock();
+        if (!mockTypes.IsEmpty)
+        {
+            INamedTypeSymbol mockTypeSymbol = mockTypes[0];
+            IAssemblySymbol? assemblySymbol = mockTypeSymbol.ContainingAssembly;
+            AssemblyIdentity identity = assemblySymbol.Identity;
+            Version version = identity.Version;
+            Version minimalVersion = new(4, 16);
+
+            if (version >= minimalVersion)
+            {
+                return;
+            }
+        }
+        
         InvocationExpressionSyntax setupInvocation = (InvocationExpressionSyntax)context.Node;
 
         if (setupInvocation.Expression is MemberAccessExpressionSyntax memberAccessExpression && context.SemanticModel.IsMoqSetupMethod(memberAccessExpression, context.CancellationToken))
