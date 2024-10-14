6efc675e9528a3e7e4d609f0bb8f13ca5915070c
Richard Murillo <rjmurillo@gmail.com>
2024-10-14 16:49:12 -0700

    Update test case commentary with data from #144

--- tests/Moq.Analyzers.Test/SetupShouldNotIncludeAsyncResultAnalyzerTests.cs
+++ tests/Moq.Analyzers.Test/SetupShouldNotIncludeAsyncResultAnalyzerTests.cs
@@ -2,14 +2,18 @@ using Verifier = Moq.Analyzers.Test.Helpers.AnalyzerVerifier<Moq.Analyzers.Setup
 
 namespace Moq.Analyzers.Test;
 
-public class SetupShouldNotIncludeAsyncResultAnalyzerTests
+public class SetupShouldNotIncludeAsyncResultAnalyzerTests(ITestOutputHelper output)
 {
     public static IEnumerable<object[]> TestData()
     {
         return new object[][]
         {
             ["""new Mock<AsyncClient>().Setup(c => c.TaskAsync());"""],
+
+            // Prior to Moq 4.16, the setup helper ReturnsAsync(), ThrowsAsync() are preferred
             ["""new Mock<AsyncClient>().Setup(c => c.GenericTaskAsync()).ReturnsAsync(string.Empty);"""],
+
+            // Starting with Moq 4.16, you can simply .Setup the returned tasks's .Result property
             ["""new Mock<AsyncClient>().Setup(c => {|Moq1201:c.GenericTaskAsync().Result|});"""],
         }.WithNamespaces().WithMoqReferenceAssemblyGroups();
     }
@@ -18,25 +22,30 @@ public class SetupShouldNotIncludeAsyncResultAnalyzerTests
     [MemberData(nameof(TestData))]
     public async Task ShouldAnalyzeSetupForAsyncResult(string referenceAssemblyGroup, string @namespace, string mock)
     {
+        string source =
+            $$"""
+              {{@namespace}}
+
+              public class AsyncClient
+              {
+                  public virtual Task TaskAsync() => Task.CompletedTask;
+              
+                  public virtual Task<string> GenericTaskAsync() => Task.FromResult(string.Empty);
+              }
+
+              internal class UnitTest
+              {
+                  private void Test()
+                  {
+                      {{mock}}
+                  }
+              }
+              """;
+
+        output.WriteLine(source);
+
         await Verifier.VerifyAnalyzerAsync(
-                $$"""
-                {{@namespace}}
-
-                public class AsyncClient
-                {
-                    public virtual Task TaskAsync() => Task.CompletedTask;
-
-                    public virtual Task<string> GenericTaskAsync() => Task.FromResult(string.Empty);
-                }
-
-                internal class UnitTest
-                {
-                    private void Test()
-                    {
-                        {{mock}}
-                    }
-                }
-                """,
+                source,
                 referenceAssemblyGroup);
     }
 }
