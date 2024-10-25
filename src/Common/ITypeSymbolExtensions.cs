3e37b658f2a67440e9e06ff51c81b6c62225e27d
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-10-25 13:21:12 -0700

    Add KnownSymbols
    WIP

---
+++ src/Common/ITypeSymbolExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Moq.Analyzers.Common;
+
+internal static class ITypeSymbolExtensions
+{
+    /// <summary>
+    /// Get the base types of a type, including the type itself.
+    /// </summary>
+    /// <remarks>
+    /// Use this to walk the inheritance chain of a type.
+    /// </remarks>
+    /// <param name="type">The <see cref="ITypeSymbol"/> to walk.</param>
+    /// <returns>The type and any inherited types.</returns>
+    public static IEnumerable<ITypeSymbol> GetBaseTypesAndThis(this ITypeSymbol type)
+    {
+        var current = type;
+        while (current is not null)
+        {
+            yield return current;
+            current = current.BaseType;
+        }
+    }
+}
---
+++ src/Common/WellKnown/KnownSymbols.cs
@@ -0,0 +1,44 @@
+﻿namespace Moq.Analyzers.Common.WellKnown;
+
+/// <summary>
+/// Main entrypoint to access well-known symbols for the analyzer.
+/// This class handles caching to prevent multiple lookups for the same symbol.
+///
+/// It returns a type derived from <see cref="ISymbol"/> in all cases. Use the
+/// <seealso cref="ISymbol.ToDisplayString(SymbolDisplayFormat?)"/> when necessary
+/// for comparisons with <seealso cref="SyntaxNode"/>s.
+/// </summary>
+internal class KnownSymbols
+{
+    public KnownSymbols(WellKnownTypeProvider typeProvider)
+    {
+        TypeProvider = typeProvider;
+    }
+
+    public KnownSymbols(Compilation compilation)
+        : this(WellKnownTypeProvider.GetOrCreate(compilation))
+    {
+    }
+
+    /// <summary>
+    /// Gets the class <c>System.Threading.Tasks.Task</c>.
+    /// </summary>
+    public INamedTypeSymbol? Task => TypeProvider.GetOrCreateTypeByMetadataName("System.Threading.Tasks.Task");
+
+    /// <summary>
+    /// Gets the class <c>System.Threading.Tasks.Task&lt;T&gt;</c>.
+    /// </summary>
+    public INamedTypeSymbol? Task1 => TypeProvider.GetOrCreateTypeByMetadataName("System.Threading.Tasks.Task`1");
+
+    /// <summary>
+    /// Gets the class <c>System.Threading.Tasks.ValueTask</c>.
+    /// </summary>
+    public INamedTypeSymbol? ValueTask => TypeProvider.GetOrCreateTypeByMetadataName("System.Threading.Tasks.ValueTask");
+
+    /// <summary>
+    /// Gets the class <c>System.Threading.Tasks.ValueTask&lt;T&gt;</c>.
+    /// </summary>
+    public INamedTypeSymbol? ValueTask1 => TypeProvider.GetOrCreateTypeByMetadataName("System.Threading.Tasks.ValueTask`1");
+
+    protected WellKnownTypeProvider TypeProvider { get; }
+}
