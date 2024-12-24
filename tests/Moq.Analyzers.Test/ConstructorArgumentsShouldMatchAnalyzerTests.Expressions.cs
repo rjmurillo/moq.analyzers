20c0af0d84316a93cb0ac5677d3d67f4a27e2e67
Richard Murillo <rjmurillo@gmail.com>
2024-12-24 09:53:35 -0800

    Add test to reproduce issue #234

---
+++ tests/Moq.Analyzers.Test/ConstructorArgumentsShouldMatchAnalyzerTests.Expressions.cs
@@ -0,0 +1,36 @@
+﻿using Verifier = Moq.Analyzers.Test.Helpers.AnalyzerVerifier<Moq.Analyzers.ConstructorArgumentsShouldMatchAnalyzer>;
+
+namespace Moq.Analyzers.Test;
+
+public partial class ConstructorArgumentsShouldMatchAnalyzerTests
+{
+    public static IEnumerable<object[]> ExpressionTestData()
+    {
+        return new object[][]
+        {
+            ["""_ = new Mock<Calculator>(() => new Calculator(), MockBehavior.Loose);"""]
+        }.WithNamespaces().WithMoqReferenceAssemblyGroups();
+    }
+
+    [Theory]
+    [MemberData(nameof(ExpressionTestData))]
+    public async Task ExpressionConstructor(string referenceAssemblyGroup, string @namespace, string mock)
+    {
+        await Verifier.VerifyAnalyzerAsync(
+            $@"
+            {@namespace}
+            public class Calculator
+            {{
+                 public int Add(int a, int b) => a + b;
+            }}
+            internal class UnitTest
+            {{
+                private void Test()
+                {{
+                    {mock}
+                }}
+            }}
+            ",
+            referenceAssemblyGroup);
+    }
+}
