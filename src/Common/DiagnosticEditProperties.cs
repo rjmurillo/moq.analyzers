1d726ab3743b7fd559612bbe6b535bb842a16a54
Richard Murillo <rjmurillo@gmail.com>
2025-06-08 18:32:09 -0700

    Refactor DiagnosticEditProperties and clean up project files
    - Removed empty lines in Common.csproj and Common.projitems.
    - Changed DiagnosticEditProperties from a record to a class.
    - Updated access modifiers for properties and methods in DiagnosticEditProperties.
    - Removed benchmark tests for DiagnosticEditProperties.

--- src/Common/DiagnosticEditProperties.cs
+++ src/Common/DiagnosticEditProperties.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace Moq.Analyzers.Common;
 
-internal record class DiagnosticEditProperties
+internal record DiagnosticEditProperties
 {
     internal static readonly string EditTypeKey = nameof(EditTypeKey);
     internal static readonly string EditPositionKey = nameof(EditPositionKey);
@@ -27,18 +27,18 @@ internal record class DiagnosticEditProperties
     /// <summary>
     /// Gets the type of edit operation to perform.
     /// </summary>
-    public EditType TypeOfEdit { get; init; }
+    internal EditType TypeOfEdit { get; init; }
 
     /// <summary>
     /// Gets the zero-based position where the edit should be applied.
     /// </summary>
-    public int EditPosition { get; init; }
+    internal int EditPosition { get; init; }
 
     /// <summary>
     /// Returns the current object as an <see cref="ImmutableDictionary{TKey, TValue}"/>.
     /// </summary>
     /// <returns>The current object as an immutable dictionary.</returns>
-    public ImmutableDictionary<string, string?> ToImmutableDictionary()
+    internal ImmutableDictionary<string, string?> ToImmutableDictionary()
     {
         ImmutableDictionary<string, string?>.Builder builder = ImmutableDictionary.CreateBuilder<string, string?>(StringComparer.Ordinal);
         builder.Add(EditTypeKey, TypeOfEdit.ToString());
@@ -52,7 +52,7 @@ internal record class DiagnosticEditProperties
     /// <param name="dictionary">The dictionary to try to convert.</param>
     /// <param name="editProperties">The output edit properties if parsing succeeded, otherwise <c>null</c>.</param>
     /// <returns><c>true</c> if parsing succeeded; <c>false</c> otherwise.</returns>
-    public static bool TryGetFromImmutableDictionary(ImmutableDictionary<string, string?> dictionary, [NotNullWhen(true)] out DiagnosticEditProperties? editProperties)
+    internal static bool TryGetFromImmutableDictionary(ImmutableDictionary<string, string?> dictionary, [NotNullWhen(true)] out DiagnosticEditProperties? editProperties)
     {
         editProperties = null;
         if (!dictionary.TryGetValue(EditTypeKey, out string? editTypeString))
