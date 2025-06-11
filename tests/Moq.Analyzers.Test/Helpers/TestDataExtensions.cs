be7f6c996f15dda0c58e31d89f7073bcc83cb6f8
Richard Murillo <rjmurillo@gmail.com>
2025-06-10 21:44:19 -0700

    Enhance test helpers with additional documentation for Moq reference assemblies
    - Added XML documentation comments to `ReferenceAssemblyCatalog` for clarity on .NET 8.0 reference assembly groups with older (4.8.2) and newer (4.18.4) versions of Moq.
    - Expanded `TestDataExtensions` with new methods and documentation to facilitate the addition of Moq reference assembly groups to test cases, improving test data clarity and usability.

--- tests/Moq.Analyzers.Test/Helpers/TestDataExtensions.cs
+++ tests/Moq.Analyzers.Test/Helpers/TestDataExtensions.cs
@@ -2,6 +2,11 @@
 
 internal static class TestDataExtensions
 {
+    /// <summary>
+    /// Adds a namespace to each test case.
+    /// </summary>
+    /// <param name="data">The test data to extend.</param>
+    /// <returns>The test data with a namespace added to each test case.</returns>
     public static IEnumerable<object[]> WithNamespaces(this IEnumerable<object[]> data)
     {
         foreach (object[] item in data)
@@ -11,6 +16,11 @@ internal static class TestDataExtensions
         }
     }
 
+    /// <summary>
+    /// Adds the reference assembly group for .NET 8.0 with an older version of Moq (4.8.2) and a newer version of Moq (4.18.4) to each test case.
+    /// </summary>
+    /// <param name="data">The test data to extend.</param>
+    /// <returns>The test data with the old and new Moq reference assembly groups added to each test case.</returns>
     public static IEnumerable<object[]> WithMoqReferenceAssemblyGroups(this IEnumerable<object[]> data)
     {
         foreach (object[] item in data)
@@ -20,6 +30,11 @@ internal static class TestDataExtensions
         }
     }
 
+    /// <summary>
+    /// Adds the reference assembly group for .NET 8.0 with an older version of Moq (4.8.2) to each test case.
+    /// </summary>
+    /// <param name="data">The test data to extend.</param>
+    /// <returns>The test data with the old Moq reference assembly group added to each test case.</returns>
     public static IEnumerable<object[]> WithOldMoqReferenceAssemblyGroups(this IEnumerable<object[]> data)
     {
         foreach (object[] item in data)
@@ -28,6 +43,11 @@ internal static class TestDataExtensions
         }
     }
 
+    /// <summary>
+    /// Adds the reference assembly group for .NET 8.0 with a newer version of Moq (4.18.4) to each test case.
+    /// </summary>
+    /// <param name="data">The test data to extend.</param>
+    /// <returns>The test data with the old Moq reference assembly group added to each test case.</returns>
     public static IEnumerable<object[]> WithNewMoqReferenceAssemblyGroups(this IEnumerable<object[]> data)
     {
         foreach (object[] item in data)
