dbc6376dd7a9641b944707b3642d3e8d42179ee2
Richard Murillo <rjmurillo@gmail.com>
2025-06-20 20:50:13 -0700

    Fixer WIP

---
+++ tests/Moq.Analyzers.Test/VerifyOverridableMembersCodeFixTests.cs
@@ -0,0 +1,266 @@
+using Verify = Moq.Analyzers.Test.Helpers.CodeFixVerifier<Moq.Analyzers.VerifyShouldBeUsedOnlyForOverridableMembersAnalyzer, Moq.Analyzers.VerifyOverridableMembersFixer>;
+
+namespace Moq.Analyzers.Test;
+
+public class VerifyOverridableMembersCodeFixTests
+{
+    public static IEnumerable<object[]> MoqReferenceAssemblyGroups() =>
+        new List<object[]>
+        {
+            new object[] { ReferenceAssemblyCatalog.Net80WithOldMoq },
+            new object[] { ReferenceAssemblyCatalog.Net80WithNewMoq },
+        };
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task MakesNonVirtualPropertyVirtual(string referenceAssemblyGroup)
+    {
+        const string test = """
+        using Moq;
+
+        public class MyClass
+        {
+            public string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => {|Moq1210:x.MyProperty|});
+            }
+        }
+        """;
+
+        const string fixtest = """
+        using Moq;
+
+        public class MyClass
+        {
+            public virtual string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.MyProperty);
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, fixtest, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task MakesNonVirtualMethodVirtual(string referenceAssemblyGroup)
+    {
+        string test = """
+        using Moq;
+
+        public class MyClass
+        {
+            public int MyMethod() => 0;
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => {|Moq1210:x.MyMethod()|});
+            }
+        }
+        """;
+
+        string fixtest = """
+        using Moq;
+
+        public class MyClass
+        {
+            public virtual int MyMethod() => 0;
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.MyMethod());
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, fixtest, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task PreservesModifiersAttributesAndDocumentation(string referenceAssemblyGroup)
+    {
+        const string test = """
+        using Moq;
+        using System;
+
+        public class MyClass
+        {
+            /// <summary>Some documentation.</summary>
+            [Obsolete]
+            protected internal string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => {|Moq1210:x.MyProperty|});
+            }
+        }
+        """;
+
+        const string fixtest = """
+        using Moq;
+        using System;
+
+        public class MyClass
+        {
+            /// <summary>Some documentation.</summary>
+            [Obsolete]
+            protected internal virtual string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.MyProperty);
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, fixtest, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task NoFixForSealedMembers(string referenceAssemblyGroup)
+    {
+        const string test = """
+        using Moq;
+
+        public class MyClass
+        {
+            public sealed override string ToString() => "";
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => {|Moq1210:x.ToString()|});
+            }
+        }
+        """;
+
+        const string fixtest = """
+        using Moq;
+
+        public class MyClass
+        {
+            public sealed override string ToString() => "";
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.ToString());
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, fixtest, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task NoFixForInterfaceMembers(string referenceAssemblyGroup)
+    {
+        string test = """
+        using Moq;
+
+        public interface IMyInterface
+        {
+            string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<IMyInterface>();
+                mock.Verify(x => x.MyProperty);
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, test, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task NoFixForAbstractMembers(string referenceAssemblyGroup)
+    {
+        string test = """
+        using Moq;
+
+        public abstract class MyClass
+        {
+            public abstract string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.MyProperty);
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, test, referenceAssemblyGroup);
+    }
+
+    [Theory]
+    [MemberData(nameof(MoqReferenceAssemblyGroups))]
+    public async Task NoFixForVirtualMembers(string referenceAssemblyGroup)
+    {
+        string test = """
+        using Moq;
+
+        public class MyClass
+        {
+            public virtual string MyProperty { get; set; }
+        }
+
+        public class MyTest
+        {
+            public void Test()
+            {
+                var mock = new Mock<MyClass>();
+                mock.Verify(x => x.MyProperty);
+            }
+        }
+        """;
+
+        await Verify.VerifyCodeFixAsync(test, test, referenceAssemblyGroup);
+    }
+}
