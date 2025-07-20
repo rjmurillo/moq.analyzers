1c893c12525f15bc9113641cbbc3f79eefad2f8d
Richard Murillo <rjmurillo@gmail.com>
2025-07-19 22:59:58 -0700

    Update benchmark configuration to include additional columns
    Import BenchmarkDotNet.Columns and modify Main method to create
    a ManualConfig that adds mean and P95 statistic columns.
    Use this custom configuration in BenchmarkSwitcher.

--- tests/Moq.Analyzers.Benchmarks/Program.cs
+++ tests/Moq.Analyzers.Benchmarks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Columns;
 
 namespace Moq.Analyzers.Benchmarks;
 
@@ -13,7 +14,14 @@ public static class Program
     /// </summary>
     /// <param name="args">Command line arguments.</param>
     public static void Main(string[] args)
-        => BenchmarkSwitcher
+    {
+        // Needed because Microsoft.CodeAnalysis.Testing does not build with optimizations. See https://github.com/dotnet/roslyn-sdk/issues/1165.
+        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator))
+            .AddColumn(StatisticColumn.Mean)
+            .AddColumn(StatisticColumn.P95);
+
+        BenchmarkSwitcher
             .FromAssembly(typeof(Program).Assembly)
-            .Run(args, DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator)); // Needed because Microsoft.CodeAnalysis.Testing does not build with optimizations. See https://github.com/dotnet/roslyn-sdk/issues/1165.
+            .Run(args, config);
+    }
 }
