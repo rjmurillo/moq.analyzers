13eac82052eef25b993daa087795d130a7819afd
Richard Murillo <rjmurillo@gmail.com>
2025-07-20 06:38:27 -0700

    refactor: remove unused Deconstruct method and unnecessary using directive in BenchmarkComparisonResult

--- src/tools/PerfDiff/BDN/DataContracts/BenchmarkComparisonResult.cs
+++ src/tools/PerfDiff/BDN/DataContracts/BenchmarkComparisonResult.cs
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
-using System.Runtime.InteropServices;
-
 namespace PerfDiff.BDN.DataContracts;
 
 /// <summary>
@@ -9,15 +7,5 @@ namespace PerfDiff.BDN.DataContracts;
 /// </summary>
 [StructLayout(LayoutKind.Auto)]
 public readonly record struct BenchmarkComparisonResult(bool CompareSucceeded, bool RegressionDetected)
-{
-    /// <summary>
-    /// Deconstructs the result into compareSucceeded and regressionDetected values.
-    /// </summary>
-    /// <param name="compareSucceeded">Indicates whether the comparison succeeded.</param>
-    /// <param name="regressionDetected">Indicates whether a regression was detected.</param>
-    public void Deconstruct(out bool compareSucceeded, out bool regressionDetected)
-    {
-        compareSucceeded = CompareSucceeded;
-        regressionDetected = RegressionDetected;
-    }
+{    
 }
