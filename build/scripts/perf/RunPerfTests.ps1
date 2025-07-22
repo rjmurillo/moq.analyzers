0727894b266f07543dcdc4a003055fa7235b5806
Richard Murillo <rjmurillo@gmail.com>
2025-07-22 00:16:08 -0700

    fix(perf): correct string interpolation for filter argument in RunPerfTests.ps1

--- build/scripts/perf/RunPerfTests.ps1
+++ build/scripts/perf/RunPerfTests.ps1
@@ -32,7 +32,7 @@ try {
             $commandArguments = "$commandArguments --profiler ETW"
         }
 
-        $commandArguments = "$commandArguments --filter '$filter'"
+        $commandArguments = "$commandArguments --filter ""$filter"""
 
         Write-Host "Invoking: dotnet $commandArguments"
 
