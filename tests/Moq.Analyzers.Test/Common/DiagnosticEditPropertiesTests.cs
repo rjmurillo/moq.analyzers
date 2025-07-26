4a5750d5b0d2907a8164554a2316269a808bb55b
Richard Murillo <rjmurillo@gmail.com>
2025-07-26 14:47:39 -0700

    test: Add comprehensive tests for DiagnosticEditProperties methods

--- tests/Moq.Analyzers.Test/Common/DiagnosticEditPropertiesTests.cs
+++ tests/Moq.Analyzers.Test/Common/DiagnosticEditPropertiesTests.cs
@@ -12,6 +12,85 @@ public class DiagnosticEditPropertiesTests
     }
 
     [Fact]
+    public void ToImmutableDictionary_AllEnumValues_RoundTrips()
+    {
+        foreach (DiagnosticEditProperties.EditType type in Enum.GetValues(typeof(DiagnosticEditProperties.EditType)))
+        {
+            DiagnosticEditProperties props = new DiagnosticEditProperties { TypeOfEdit = type, EditPosition = 0 };
+            ImmutableDictionary<string, string?> dict = props.ToImmutableDictionary();
+            Assert.Equal(type.ToString(), dict[DiagnosticEditProperties.EditTypeKey]);
+            Assert.Equal("0", dict[DiagnosticEditProperties.EditPositionKey]);
+            Assert.True(DiagnosticEditProperties.TryGetFromImmutableDictionary(dict, out DiagnosticEditProperties? roundTripped));
+            Assert.Equal(type, roundTripped!.TypeOfEdit);
+        }
+    }
+
+    [Fact]
+    public void ToImmutableDictionary_HandlesNegativeAndLargeEditPosition()
+    {
+        DiagnosticEditProperties propsNeg = new DiagnosticEditProperties { TypeOfEdit = DiagnosticEditProperties.EditType.Insert, EditPosition = -1 };
+        ImmutableDictionary<string, string?> dictNeg = propsNeg.ToImmutableDictionary();
+        Assert.Equal("-1", dictNeg[DiagnosticEditProperties.EditPositionKey]);
+        Assert.True(DiagnosticEditProperties.TryGetFromImmutableDictionary(dictNeg, out DiagnosticEditProperties? roundTrippedNeg));
+        Assert.Equal(-1, roundTrippedNeg!.EditPosition);
+
+        DiagnosticEditProperties propsLarge = new DiagnosticEditProperties { TypeOfEdit = DiagnosticEditProperties.EditType.Replace, EditPosition = int.MaxValue };
+        ImmutableDictionary<string, string?> dictLarge = propsLarge.ToImmutableDictionary();
+        Assert.Equal(int.MaxValue.ToString(), dictLarge[DiagnosticEditProperties.EditPositionKey]);
+        Assert.True(DiagnosticEditProperties.TryGetFromImmutableDictionary(dictLarge, out DiagnosticEditProperties? roundTrippedLarge));
+        Assert.Equal(int.MaxValue, roundTrippedLarge!.EditPosition);
+    }
+
+    [Fact]
+    public void TryGetFromImmutableDictionary_IgnoresExtraKeys()
+    {
+        ImmutableDictionary<string, string?> dict = ImmutableDictionary<string, string?>.Empty
+            .Add(DiagnosticEditProperties.EditTypeKey, "Insert")
+            .Add(DiagnosticEditProperties.EditPositionKey, "1")
+            .Add("ExtraKey", "ExtraValue");
+        Assert.True(DiagnosticEditProperties.TryGetFromImmutableDictionary(dict, out DiagnosticEditProperties? props));
+        Assert.Equal(DiagnosticEditProperties.EditType.Insert, props!.TypeOfEdit);
+        Assert.Equal(1, props.EditPosition);
+    }
+
+    [Fact]
+    public void DiagnosticEditProperties_EqualityAndImmutability()
+    {
+        DiagnosticEditProperties a = new DiagnosticEditProperties { TypeOfEdit = DiagnosticEditProperties.EditType.Insert, EditPosition = 1 };
+        DiagnosticEditProperties b = new DiagnosticEditProperties { TypeOfEdit = DiagnosticEditProperties.EditType.Insert, EditPosition = 1 };
+        DiagnosticEditProperties c = new DiagnosticEditProperties { TypeOfEdit = DiagnosticEditProperties.EditType.Replace, EditPosition = 1 };
+        Assert.Equal(a, b);
+        Assert.NotEqual(a, c);
+        Assert.True(a.Equals(b));
+        Assert.False(a.Equals(c));
+        Assert.Equal(a.GetHashCode(), b.GetHashCode());
+    }
+
+    [Fact]
+    public void TryGetFromImmutableDictionary_ParsesCultureInvariant()
+    {
+        // Use reflection to set CurrentCulture to avoid RS1035 banned symbol in test context
+        System.Type cultureType = typeof(System.Globalization.CultureInfo);
+        System.Reflection.PropertyInfo? currentCultureProperty = cultureType.GetProperty("CurrentCulture");
+        object? originalCulture = currentCultureProperty!.GetValue(null);
+        try
+        {
+            System.Globalization.CultureInfo frCulture = new System.Globalization.CultureInfo("fr-FR");
+            currentCultureProperty.SetValue(null, frCulture);
+            ImmutableDictionary<string, string?> dict = ImmutableDictionary<string, string?>.Empty
+                .Add(DiagnosticEditProperties.EditTypeKey, "Insert")
+                .Add(DiagnosticEditProperties.EditPositionKey, "1234");
+            DiagnosticEditProperties? props;
+            Assert.True(DiagnosticEditProperties.TryGetFromImmutableDictionary(dict, out props));
+            Assert.Equal(1234, props!.EditPosition);
+        }
+        finally
+        {
+            currentCultureProperty.SetValue(null, originalCulture);
+        }
+    }
+
+    [Fact]
     public void TryGetFromImmutableDictionary_Succeeds_WithValidDictionary()
     {
         ImmutableDictionary<string, string?> dict = ImmutableDictionary<string, string?>.Empty
