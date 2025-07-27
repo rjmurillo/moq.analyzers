63d39b66ad6a50de46769a02bf1f380dd43582e7
Richard Murillo <rjmurillo@gmail.com>
2025-07-26 19:29:41 -0700

    test: Update test data for RedundantTimesSpecificationAnalyzer to include additional cases for Times usage

--- tests/Moq.Analyzers.Test/RedundantTimesSpecificationAnalyzerTests.cs
+++ tests/Moq.Analyzers.Test/RedundantTimesSpecificationAnalyzerTests.cs
@@ -10,32 +10,62 @@ public class RedundantTimesSpecificationAnalyzerTests(ITestOutputHelper output)
         IEnumerable<object[]> both = new object[][]
         {
             // Should detect redundant Times.AtLeastOnce()
-            ["""new Mock<ISampleInterface>().Verify(x => x.TestMethod(), {|Moq1420:Times.AtLeastOnce()|});"""],
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), {|Moq1420:Times.AtLeastOnce()|});"],
+
+            // Should NOT detect redundant for Times.AtMostOnce()
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.AtMostOnce());"],
+
+            // Should NOT detect redundant for Times.AtLeast(1)
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.AtLeast(1));"],
+
+            // Should NOT detect redundant for Times.AtMost(1)
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.AtMost(1));"],
 
             // Should NOT detect other Times specifications
-            ["""new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Never());"""],
-            ["""new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Once());"""],
-            ["""new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Exactly(3));"""],
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Never());"],
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Once());"],
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Exactly(3));"],
+
+            // Should NOT detect variable/expression-based Times
+            ["int times = 2; new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.Exactly(times));"],
+            ["int times = 2; new Mock<ISampleInterface>().Verify(x => x.TestMethod(), Times.AtLeast(times));"],
 
             // Should NOT detect Verify calls without Times parameter (default behavior)
-            ["""new Mock<ISampleInterface>().Verify(x => x.TestMethod());"""],
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod());"],
 
             // Should detect redundant Times.AtLeastOnce() in VerifyGet
-            ["""new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, {|Moq1420:Times.AtLeastOnce()|});"""],
+            ["new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, {|Moq1420:Times.AtLeastOnce()|});"],
+
+            // Should NOT detect redundant for Times.AtMostOnce() in VerifyGet
+            ["new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, Times.AtMostOnce());"],
 
             // Should NOT detect other Times in VerifyGet
-            ["""new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, Times.Never());"""],
-            ["""new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty);"""],
+            ["new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, Times.Never());"],
+            ["new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty);"],
+
+            // Should NOT detect variable/expression-based Times in VerifyGet
+            ["int times = 2; new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, Times.Exactly(times));"],
+
+            // Should detect redundant Times with message argument
+            ["new Mock<ISampleInterface>().Verify(x => x.TestMethod(), {|Moq1420:Times.AtLeastOnce()|}, \"should be called at least once\");"],
+            ["new Mock<ISampleInterface>().VerifyGet(x => x.TestProperty, {|Moq1420:Times.AtLeastOnce()|}, \"should be called at least once\");"],
+
         }.WithNamespaces().WithMoqReferenceAssemblyGroups();
 
         IEnumerable<object[]> newMoqOnly = new object[][]
         {
             // Should detect redundant Times.AtLeastOnce() in VerifySet (only available in new Moq versions)
-            ["""new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, {|Moq1420:Times.AtLeastOnce()|});"""],
+            ["new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, {|Moq1420:Times.AtLeastOnce()|});"],
+
+            // Should NOT detect redundant for Times.AtMostOnce() in VerifySet
+            ["new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, Times.AtMostOnce());"],
 
             // Should NOT detect other Times in VerifySet
-            ["""new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, Times.Never());"""],
-            ["""new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); });"""],
+            ["new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, Times.Never());"],
+            ["new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); });"],
+
+            // Should NOT detect variable/expression-based Times in VerifySet
+            ["int times = 2; new Mock<ISampleInterface>().VerifySet(x => { x.TestProperty = It.IsAny<string>(); }, Times.Exactly(times));"],
         }.WithNamespaces().WithNewMoqReferenceAssemblyGroups();
 
         return both.Concat(newMoqOnly);
