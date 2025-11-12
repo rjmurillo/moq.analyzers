a82248f6aed6cf1270152a71e8bada15a4a62a7a
Richard Murillo <rimuri@microsoft.com>
2025-11-12 11:13:29 -0800

    refactor(tests): change GetPackages method to return TheoryData for improved test data handling

--- tests/Moq.Analyzers.Test/PackageTests.cs
+++ tests/Moq.Analyzers.Test/PackageTests.cs
@@ -4,17 +4,20 @@ namespace Moq.Analyzers.Test;
 
 public class PackageTests
 {
-    public static IEnumerable<object[]> GetPackages()
+    public static TheoryData<FileInfo> GetPackages()
     {
         DirectoryInfo directory = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory!;
         FileInfo[] packages = directory.GetFiles("Moq.Analyzers*.nupkg")
             .OrderBy(fileInfo => fileInfo.Name, StringComparer.Ordinal)
             .ToArray();
 
+        TheoryData<FileInfo> theoryData = new();
         foreach (FileInfo package in packages)
         {
-            yield return [package];
+            theoryData.Add(package);
         }
+
+        return theoryData;
     }
 
     [Theory]
